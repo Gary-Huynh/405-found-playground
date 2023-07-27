b64f9ec950a0b4041d4966191f001510e6f49f45
crissu91 <cristinaf.stefan@gmail.com>
2023-07-27 10:22:15 +0100

    first component in svelte

---
+++ src/ShoppingList.svelte
@@ -0,0 +1,16 @@
+<script>
+    let items = [{name: "bananas", qty: 4}, {name: "rice", qty: 1}, {name: "chicken", qty: 2},{name: "onions", qty: 5}]
+</script>
+
+<h1>Shopping list</h1>
+<ul>
+  {#each items as item}
+    <li>{item.name} x {item.qty}</li>
+  {/each}
+</ul>
+
+<style>
+    li {
+      color: red;
+    }
+</style>
